23642272596d45681c37216670d28a827c86905b
137megumi <shiroyamatakayuki19840408@gmail.com>
2020-06-16 15:39:53 +0900

    Viewへの値セット

---
+++ HelloController.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * /app/Controller/HelloController.php
+ */
+class HelloController extends AppController
+{
+
+    /**自動レンダリング無効　*/
+    public $autoLayout = false;
+
+    // 利用するモデルを指定する
+    public $uses = array('Inputform');
+    /**
+     * indexアクション
+     */
+    public function index() {}
+
+    /**
+     * otherアクション
+     */
+    public function other() {
+
+        //リクエストがPOSTメソッドで送られてきた場合
+        if($this->request->is('post')) {
+
+            //formパラメーターの取得
+            $post = $this->request->data;
+
+            //viewへ値のセット
+            $this->set('post', $post);
+        }
+    }
+}
